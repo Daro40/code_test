f996c3020b9aa24a2899e940461820a25c1f2dd7
Daro40 <daroforti@gmail.com>
2021-03-05 02:33:16 -0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -15,5 +15,5 @@ def test_zeros_to_end(test_input,expected):
     # Given an array of numbers, write a function to move all zeroes to the end of it while maintaining the relative order of the non-zero elements.
     #arr = [0, 3, 4, 6, 21, 0, 3, 0, 0, 2, 4, 1, 5, 7, 8, 0, 3234, 2534, 0, 524, 65, 42, 1, 6, 0, 788, 65, 7, 7]
     #expected = [3, 4, 6, 21, 3, 2, 4, 1, 5, 7, 8, 3234, 2534, 524, 65, 42, 1, 6, 788, 65, 7, 7, 0, 0, 0, 0, 0, 0, 0]
-    fuck = zeros_to_end(test_input, len(test_input))
-    assert np.array_equal(np.array(fuck), np.array(expected))
+    processed_array = zeros_to_end(test_input, len(test_input))
+    assert np.array_equal(np.array(processed_array), np.array(expected))
